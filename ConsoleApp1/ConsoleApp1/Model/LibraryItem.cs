d0da87b0704c83b1ba09e3298e32bf3ba321dbfc
kosaattila <kosa.attila22i@egressy.info>
2026-09-10 12:28:50 +0200

    Magazine feladatok

--- ConsoleApp1/ConsoleApp1/Model/LibraryItem.cs
+++ ConsoleApp1/ConsoleApp1/Model/LibraryItem.cs
@@ -9,12 +9,15 @@ namespace ConsoleApp1.Model
     public class LibraryItem
     {
         public string Title { get; set; }
+
+        public static int count;
         public bool IsAvailable { get; set; }
 
         public LibraryItem(string title) 
         {
             IsAvailable = true;
             Title = title;
+            count++;
         }
 
         public bool Borrow()
---
+++ ConsoleApp1/ConsoleApp1/Model/Magazine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class Magazine : LibraryItem
+    {
+        public int IssueNumber { get; set; }
+        
+        public Magazine(string title) : base(title)
+        {
+
+        }
+
+        public override string GetDescription()
+        {
+            return $"asd {Title} {IssueNumber}";
+        }
+    }
+}
