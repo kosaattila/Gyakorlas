8526edbfd8182eb021e884556f989fbebad45ff2
kosaattila <kosa.attila22i@egressy.info>
2026-09-10 12:12:25 +0200

    Book osztaly gg

--- ConsoleApp1/ConsoleApp1/Model/Book.cs
+++ ConsoleApp1/ConsoleApp1/Model/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,9 +33,19 @@ namespace ConsoleApp1.Model
             PageCount = pageCount;
         }
 
+        public Book(string title , string author) : this(title,author,0)
+        {
+
+        }
+
         public override string GetDescription()
         {
             return $"Title: {Title} - Author: {Author} - {PageCount}";
+        }
+
+        public bool IsLong()
+        {
+            return PageCount > 300;
         }
     }
 }
