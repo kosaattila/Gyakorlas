901ed5dc3f2ff4fe3ccf7caea7e3c3440ddf8a08
kosaattila <kosa.attila22i@egressy.info>
2026-09-10 12:04:59 +0200

    Added skeleton

---
+++ ConsoleApp1/ConsoleApp1/Model/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class Book : LibraryItem
+    {
+        public string Author { get; set; }
+        private int _pageCount;
+
+        public int PageCount
+        {
+            get { return _pageCount; }
+            set {
+                if (value < 0) 
+                {
+                    _pageCount = 0;       
+                }
+                else
+                {
+                    _pageCount = value;
+                }
+            }            
+        }
+
+        public Book(string title, string author, int pageCount) : base(title)
+        {
+            Author = author;
+            PageCount = pageCount;
+        }
+
+        public override string GetDescription()
+        {
+            return $"Title: {Title} - Author: {Author} - {PageCount}";
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Model/LibraryItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class LibraryItem
+    {
+        public string Title { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public LibraryItem(string title) 
+        {
+            IsAvailable = true;
+            Title = title;
+        }
+
+        public bool Borrow()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Return()
+        {
+            IsAvailable = true;
+        }
+
+        public virtual string GetDescription()
+        {
+            return Title;
+        }
+    }
+}
